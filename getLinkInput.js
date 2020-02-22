609c1d35d11f353c7f5c444ad4cd6420df77ce28
shc0743 <60457042+shc0743@users.noreply.github.com>
2020-02-22 16:39:13 +0800

    Create index.html

---
+++ getLinkInput.js
@@ -0,0 +1,39 @@
+/********
+DATA=2020-02-22
+LASTDATA=2020-02-22
+METHOD=GET
+QUOTE=<script src="https://shc7432.github.io/public/getLinkInput.js"></script>
+QUOTEOR=<script src="https://shc0743.github.io/getLinkInput.js"></script>
+********/
+
+function findInput(name,succeed,error){try{
+eval(name+"=undefined")
+var path=location.href
+if(path.search("\\?")!==-1){
+var inputContent=path.split("?")[1]
+if(inputContent.search("&")!==-1) { 
+inputContent=inputContent.split("&") 
+var i=0;/*/SUM=7/*/
+for(;i<inputContent.length;i++){
+if(eval("/"+name+"=/.test(inputContent[i])")){
+eval(name+"=inputContent[i].replace("+'"'+name+"=","");
+break;
+ }
+}
+} else {
+if(inputContent.search(eval('/'+name+'=/')!==-1){
+eval(name+'=inputContent.replace(/'+name+'=/,"")'
+ }
+}
+  }
+
+if(name!==undefined){
+if(typeof(succeed)=="function") {return succeed()} else
+if(typeof(succeed)=="function") {return eval(succeed)} else
+return eval(name);
+ } else {
+if(typeof(error)=="function") {return error()} else
+if(typeof(error)=="function") {return eval(error)} else
+return eval(name);
+ }
+}catch(err){alert(err)}})()
