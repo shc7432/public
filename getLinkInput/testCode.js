e7259d0166c3a09a9e199cd83983635398fa1fd1
shc0743 <60457042+shc0743@users.noreply.github.com>
2020-02-24 15:52:37 +0800

    Update and rename testHex&testCode.js to testCode.js

--- getLinkInput/testCode.js
+++ getLinkInput/testCode.js
@@ -1,12 +1,5 @@
 (function(){
-var hexscript=document.querySelectorAll('script[type="hex/js"]')
 var i
-for(i=0;i<hexscript.length;i++){
-try{
-eval(hexscript[i].innerHTML)
-}
-catch(err){document.body.innerHTML+="<br>"+err;console.log(err)}
-}
 var testscript=document.querySelectorAll('script[type="test/js"]')
 for(i=0;i<testscript.length;i++){
 try{
