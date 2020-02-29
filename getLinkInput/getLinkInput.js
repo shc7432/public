955660dbe4e6b42f8ad53e69046ed0bf7c6456c7
shc0743 <60457042+shc0743@users.noreply.github.com>
2020-02-29 15:27:34 +0800

    Update getLinkInput.js

--- getLinkInput/getLinkInput.js
+++ getLinkInput/getLinkInput.js
@@ -20,3 +20,13 @@ getUrlValue = function (name) {
         }
         return null;
     }
+(function(){
+var i
+var testscript=document.querySelectorAll('script[type="test/js"]')
+for(i=0;i<testscript.length;i++){
+try{
+eval(testscript[i].innerHTML)
+}
+catch(err){document.body.innerHTML+="<br>"+err;console.log(err)}
+}
+})()
