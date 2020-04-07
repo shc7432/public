e9ca6e6894d60c57e3c79d44641d2a80aec5a3f3
shc0743 <60457042+shc0743@users.noreply.github.com>
2020-04-07 11:10:38 +0800

    Update sometool.js

--- something/sometool.js
+++ something/sometool.js
@@ -106,13 +106,32 @@ document.execCommand('copy');
 return true;
 }
 
-(function(){
-/*/Import JavaScript/*/
-
-let script1 = document.createElement('script');
-script1.src = "https://shc7432.github.io/public/something/other/password.js"
-document.documentElement.append(script1);
-
-})
+window.aesPwAdd=(function(){
+if(!window.CryptoJS) return false;
+return function(){
+var plaintText = arguments[0];
+var keyStr = arguments[1];
+var key = CryptoJS.enc.Utf8.parse(keyStr);
+var encryptedData = CryptoJS.AES.encrypt(plaintText, key, {
+mode: CryptoJS.mode.ECB,
+padding: CryptoJS.pad.Pkcs7
+});
+var encryptedBase64Str = encryptedData.toString();
+console.log(encryptedBase64Str);
+return encryptedData.ciphertext.toString();
+}
+})()
+window.aesPwUnAdd=(function(){
+if(!window.CryptoJS) return false;
+return function(){
+var encryptedHexStr = CryptoJS.enc.Hex.parse(arguments[0]);
+var encryptedBase64Str = CryptoJS.enc.Base64.stringify(encryptedHexStr);
+var decryptedData = CryptoJS.AES.decrypt(encryptedBase64Str, key, {
+mode: CryptoJS.mode.ECB,
+padding: CryptoJS.pad.Pkcs7
+});
+return decryptedData.toString(CryptoJS.enc.Utf8);
+}
+})()
 
 })()
