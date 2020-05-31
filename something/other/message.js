958a2940fbbcc306c12f17f3d4522c365debfbd8
shc0743 <60457042+shc0743@users.noreply.github.com>
2020-05-31 16:32:38 +0800

    Update message.js

--- something/other/message.js
+++ something/other/message.js
@@ -19,17 +19,19 @@ z-index: 1;
 overflow: auto;
 }
 </style>`
-
 window.createMessage=window.showMessage=function(text,obj){
-msgelem.innerHTML+="<span class=inJavaScriptFunction"+
-"CreateMessageMessageElementStyle></span>"
-var elem=document.querySelectorAll(
-".inJavaScriptFunctionCreateMessageMessageElementStyle");
-elem=elem[elem.length-1];
-if(typeof obj != "object") obj={};
-elem.style.backgroundColor=(obj.bgcolor||obj.backgroundColor||"#000000");
-elem.style.color=(obj.color||"#ffffff");
-
+  msgelem.innerHTML+="<span class=inJavaScriptFunction"+
+  "CreateMessageMessageElementStyle></span>"
+  var elem=document.querySelectorAll(
+  ".inJavaScriptFunctionCreateMessageMessageElementStyle");
+  elem=elem[elem.length-1];
+  if(typeof obj != "object") obj={};
+  elem.style.backgroundColor=(obj.bgcolor||obj.backgroundColor||"#000000");
+  elem.style.color=(obj.color||"#ffffff");
+  elem.style.left=(obj.x||obj.left||0);
+  elem.style.top=(obj.y||obj.top||0);
+  elem.innerHTML=(text||obj.text||obj.content||obj.innerHTML||"")
+  
 }
 
 })()
