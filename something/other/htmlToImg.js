bc796a57d2fac250f84d3ec9d75fb6c69488874a
shc0743 <60457042+shc0743@users.noreply.github.com>
2020-06-26 10:30:42 +0800

    Update and rename textToImg.js to htmlToImg.js

--- something/other/htmlToImg.js
+++ something/other/htmlToImg.js
@@ -1,11 +1,13 @@
-/**
+/*
+
+**
 * js使用canvas将文字转换成图像数据base64
 * @param {string}    text              文字内容  "abc"
 * @param {string}    fontsize          文字大小  20
 * @param {function}  fontcolor         文字颜色  "#000"
 * @param {boolean}   imgBase64Data     图像数据
-*/
-textBecomeImg: function (text,fontsize,fontcolor){
+*
+textBecomeImg= function (text,fontsize,fontcolor){
     var canvas = document.createElement('canvas');
     //小于32字加1  小于60字加2  小于80字加4    小于100字加6
     $buHeight = 0;
@@ -24,21 +26,16 @@ textBecomeImg: function (text,fontsize,fontcolor){
     //top（顶部对齐） hanging（悬挂） middle（中间对齐） bottom（底部对齐） alphabetic是默认值
     context.textBaseline = 'middle';
     context.fillText(text,0,fontsize/2)
- 
     //如果在这里直接设置宽度和高度会造成内容丢失 , 暂时未找到原因 , 可以用以下方案临时解决
     //canvas.width = context.measureText(text).width;
- 
- 
     //方案一：可以先复制内容  然后设置宽度 最后再黏贴   
     //方案二：创建新的canvas,把旧的canvas内容黏贴过去 
     //方案三： 上边设置完宽度后，再设置一遍文字
- 
     //方案一： 这个经过测试有问题，字体变大后，显示不全,原因是canvas默认的宽度不够，
     //如果一开始就给canvas一个很大的宽度的话，这个是可以的。   
     //var imgData = context.getImageData(0,0,canvas.width,canvas.height);  //这里先复制原来的canvas里的内容
     //canvas.width = context.measureText(text).width;  //然后设置宽和高   
     //context.putImageData(imgData,0,0); //最后黏贴复制的内容
- 
     //方案三：改变大小后，重新设置一次文字
     canvas.width = context.measureText(text).width;
     context.fillStyle = fontcolor;
@@ -48,4 +45,39 @@ textBecomeImg: function (text,fontsize,fontcolor){
  
     var dataUrl = canvas.toDataURL('image/png');//注意这里背景透明的话，需要使用png
     return dataUrl;
+}
+*/
+window.loadjs=window.loadJS=(function(){
+    var head = document.getElementsByTagName('head')[0]; 
+    var script= document.createElement("script"); 
+    script.type = "text/javascript"; 
+    script.src=arguments[0]; 
+    head.appendChild(script); 
+    if(/ie/i.test(navigator.userAgent)) return script;
+    script.onload=arguments[1];
+    if(typeof arguments[1] != "function" ) script.onload=undefined;
+    return script;
+})
+loadjs("https://html2canvas.hertzen.com/dist/html2canvas.js")
+window.htmlToImg=function(){
+    if(typeof arguments[0] == "object") {var _canvas=arguments[0]} else retutn false;
+    var canvas2 = document.createElement("canvas");
+    var w = parseInt(window.getComputedStyle(_canvas).width);
+    var h = parseInt(window.getComputedStyle(_canvas).height);
+    //将canvas画布放大若干倍，然后盛放在较小的容器内，就显得不模糊了
+    canvas2.width = w * 2;
+    canvas2.height = h * 2;
+    canvas2.style.width = w + "px";
+    canvas2.style.height = h + "px";
+    //可以按照自己的需求，对context的参数修改,translate指的是偏移量
+    //  var context = canvas.getContext("2d");
+    //  context.translate(0,0);
+    var context = canvas2.getContext("2d");
+    context.scale(2, 2);
+    var arg1=arguments[1];
+    html2canvas(arguments[0], { canvas: canvas2 }).then(function(canvas) {
+    var src=canvas.toDataURL();
+    if(typeof arg1 == "function") arg1();
+    });
+    
 }
