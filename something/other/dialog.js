fba4bd0cbf00f23e31c90bc74ae2291fc6f74538
shc0743 <60457042+shc0743@users.noreply.github.com>
2020-07-08 20:13:36 +0800

    Update dialog.js

--- something/other/dialog.js
+++ something/other/dialog.js
@@ -3,6 +3,13 @@
 ****/
 (function(){
 var d=function(elem,set){
+elem.style.position="absolute";
+elem.style.left="50%";
+elem.style.top="50%";
+elem.style.marginLeft="-50%";
+elem.style.marginTop="-50%";
+elem.style.backgroundColor=(set.backgroundColor||
+set.bgcolor||"#fff");
 
 };
 window.dialog=window.Dialog=d;
