62cfc313416d2e75271cbbf94f937d6d2da074c0
shc0743 <60457042+shc0743@users.noreply.github.com>
2020-07-09 11:45:08 +0800

    Update dialog.js

--- something/other/dialog.js
+++ something/other/dialog.js
@@ -2,7 +2,28 @@
 233
 ****/
 (function(){
-var d=function(elem,set){
+/*
+Code:
+========
+.login{/**样式的名字 对应class=xxx**/
+width:200px;/**宽度**/
+height:100px;/**高度**/
+position:absolute;/**绝对定位**/
+left:50%;/**左边50%**/
+top:50%;/**顶部50%**/
+margin-top:-50px;/**上移-50%**/
+margin-left:-100px;/**左移-50%**/
+}
+========
+*/
+function d(elem,set){
+//set return value
+var rtn={};
+rtn.oSet=elem.innerHTML;
+rtn.destroy=function(){
+elem.innerHTML=this.oSet;
+}
+//set styles
 elem.style.position="absolute";
 elem.style.left="50%";
 elem.style.top="50%";
@@ -10,6 +31,12 @@ elem.style.marginLeft="-50%";
 elem.style.marginTop="-50%";
 elem.style.backgroundColor=(set.backgroundColor||
 set.bgcolor||"#fff");
+elem.style.width=(set.width||screen.width-200+"px");
+elem.style.height=(set.height||"auto");
+elem.style.zIndex=(set.zIndex||set.zindex||"5");
+//set model
+
+//set others
 
 };
 window.dialog=window.Dialog=d;
