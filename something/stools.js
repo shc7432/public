e1c6cd298570facc8b43a266be71469848efb937
shc0743 <shc0743@outlook.com>
2020-08-28 18:27:36 +0800

    Update stools.js

--- something/stools.js
+++ something/stools.js
@@ -5,6 +5,17 @@ CreateDate: 2020-08-28
 LastUploadDate: 2020-08-28
 ========
 ****/
-(function(o){
-
-})(new Object())
+(function(newobject,w,d,run,norun){
+//Verify parameter
+if(!(typeof(newobject)=="object"&w==window&d==
+document&run&(!norun))) return false;
+//End #Verify parameter#
+function loadjs(src) {
+  let script = document.createElement('script');
+  script.src = src;
+  script.async = false;
+  document.head.appendChild(script);
+}
+w.sTools=new Object();
+w.gadgetsInDomainShc7432=new Object();
+})(new Object(),window,document,1,0)
