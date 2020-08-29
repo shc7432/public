9cf2ee776a87d1e8879f1b98660724ca4fc25bb5
shc0743 <shc0743@outlook.com>
2020-08-29 21:06:37 +0800

    Update stools.js

--- something/stools.js
+++ something/stools.js
@@ -12,7 +12,13 @@ LastUpdataDate: 2020-08-28
 if(!(typeof(newobject)=="object"&w==window&d==
 document&run&(!norun))) return false;
 //End #Verify parameter#
-function loadjs(a,b) {
+function loadjs(a,b,c,d) {
+/**
+@a src or config
+@b config
+@c callback
+@d error handler
+**/
   let script = document.createElement('script');
   script.src = typeof(a)=="string" ? a : typeof(a)==
   "object" ? (a.src||a.href||"PLACEHOLDER") : typeof(
@@ -21,7 +27,19 @@ function loadjs(a,b) {
   script.async = typeof(a)==
   "object" ? (a.async||false) : typeof(b)=='object' ?
   (b.async||false) : false;
+  //ONLOAD
+  script.onload= typeof(a)=='object' ? ((a.callback||
+  a.onload||false) ? (a.callback||a.onload) : 
+  undefined) : typeof(b)=='object' ? ((b.callback||
+  b.onload||false) ? (b.callback||b.onload) : 
+  undefined) : typeof(c)=='function' ? c : undefined;
+  //ONERROR
+  script.onerror= typeof(a)=='object' ? ((a.onerror||
+  false) ? a.onerror : undefined) : typeof(b)==
+  'object' ? ((b.onerror||false) ? b.onerror) : 
+  undefined) : typeof(d)=='function' ? d : undefined;
   document.head.appendChild(script);
+  return script;
 }
 var s=function(IMPORT_FUNCTION){
 const RUN_FUNCTION=IMPORT_FUNCTION?function(){
@@ -41,11 +59,12 @@ get() {
   return function(){
     return delete window.sTools ? this : false ;
   }
-}
+},
 set(val) {}
 })
 s.prototype.loadjs=loadjs;
 //load JavaScript
+const JS_DOMAIN="https://shc7432.github.io";
 loadjs();
 w.sTools=w.gadgetsInDomainShc7432=s;
 })(new Object(),window,document,1,0)
