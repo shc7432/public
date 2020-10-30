48e7e23c83d954cac92ee71b1d562668c3d44cf1
shc0743 <shc0743@outlook.com>
2020-10-30 17:36:10 +0800

    Update view-source.js

--- something/other/view-source.js
+++ something/other/view-source.js
@@ -1,11 +1,13 @@
 (function(){
 var i=new Date().getTime();
 if(confirm('View method?\nYes -- in new window\nNo -- in this window')){
-window.open('data:text/html;charset=utf-8,<meta name=viewport content="width=device-width">'+'<textarea id=v'+
+let w=window.open();w.document.open();
+w.document.write('<meta name=viewport content="width=device-width">'+'<textarea id=v'+
 ' style="z-index:1;position:fixed;left:0;top:0;width:100%;height:100%">'+
-document.documentElement.innerHTML.replace(new RegExp('</textarea>','igm'),'</text area>')+
 '</textarea><span style="position:fixed;top:0;right:0;background-color:#fff;z-index:2;border:1px solid #ccc">'+
-'<!--a onclick="window.opener.document.documentElement.innerHTML=v.value">Save</a> | --><a onclick="window.close()">Close | x</a></span>');
+'<!--a onclick="window.opener.document.documentElement.innerHTML=v.value">Save</a> | -->'+
+'<a onclick="window.close()">Close | x</a></span>');w.document.getElementById('v').value=
+document.documentElement.innerHTML.replace(new RegExp('</textarea>','igm'),'</text area>');
 return true};var a=document.createElement('div');
 a.innerHTML=('<textarea id=wetxtasorgcodeelem'+i+
 ' style="background-color:#fff;z-index:2147483640;position:fixed;left:0;top:0;width:100%;height:50%">'+
