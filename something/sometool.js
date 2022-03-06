8b3c860802b7b6c7a5d15d183d219db44b4f3815
shc0743 <shc0743@outlook.com>
2022-03-06 15:00:20 +0800

    navigator.clipboard

--- something/sometool.js
+++ something/sometool.js
@@ -63,60 +63,67 @@ https://download.sysinternals.com/files/PSTools.zip
 */
 
 window.copytext=function(text){
-if(typeof document.execCommand!=="function"){
-//alert("复制失败，请长按复制");
-return false;
-}
-var dom = document.createElement("textarea");
-dom.value = text;
-dom.setAttribute('style', 'display: block;width: 1px;height: 1px;');
-if(document.body){
-document.body.appendChild(dom);
-} else if(document.documentElement){
-document.documentElement.appendChild(dom);
-} else {
-//alert("复制失败，请长按复制");
-return false;
-}
-dom.select();
- var result = document.execCommand('copy');
-if(document.body){
-document.body.removeChild(dom);
-} else if(document.documentElement){
-document.documentElement.removeChild(dom);
-} else {
-//alert("复制失败，请长按复制");
-return false;
-}
-if (result) {
-//alert("复制成功");
-return true;
-}
-if(typeof document.createRange!=="function"){
-//alert("复制失败，请长按复制");
-return false;
-}
-var range = document.createRange();
-var div=document.createElement('div');
-div.innerHTML=text;
-div.setAttribute('style', 'height: 1px;fontSize: 1px;overflow: hidden;');
-if(document.body){
-document.body.appendChild(div);
-} else if(document.documentElement){
-document.documentElement.appendChild(div);
-} else {
-//alert("复制失败，请长按复制");
-return false;
-}
-range.selectNode(div);
-const selection = window.getSelection();
-if (selection.rangeCount > 0){
-selection.removeAllRanges();
-}
-selection.addRange(range);
-document.execCommand('copy');
-//alert("复制成功")
-return true;
+     /* support for new browsers */
+    if (window.navigator.clipboard) {
+        return window.navigator.clipboard.writeText(text);
+    }
+    
+    /* supports for old browsers */ {
+    if (typeof document.execCommand !== "function") {
+        //alert("复制失败，请长按复制");
+        return false;
+    }
+    var dom = document.createElement("textarea");
+    dom.value = text;
+    dom.setAttribute('style', 'position:absolute;top:-1000%;');
+    if (document.body) {
+        document.body.appendChild(dom);
+    } else if (document.documentElement) {
+        document.documentElement.appendChild(dom);
+    } else {
+        //alert("复制失败，请长按复制");
+        return false;
+    }
+    dom.select();
+    var result = document.execCommand('copy');
+    if (document.body) {
+        document.body.removeChild(dom);
+    } else if (document.documentElement) {
+        document.documentElement.removeChild(dom);
+    } else {
+        //alert("复制失败，请长按复制");
+        return false;
+    }
+    if (result) {
+        //alert("复制成功");
+        return true;
+    }
+    if (typeof document.createRange !== "function") {
+        //alert("复制失败，请长按复制");
+        return false;
+    }
+    var range = document.createRange();
+    var div = document.createElement('div');
+    div.innerHTML = text;
+    div.setAttribute('style', 'position:absolute;top:-1000%;');
+    if (document.body) {
+        document.body.appendChild(div);
+    } else if (document.documentElement) {
+        document.documentElement.appendChild(div);
+    } else {
+        //alert("复制失败，请长按复制");
+        return false;
+    }
+    range.selectNode(div);
+    const selection = window.getSelection();
+    if (selection.rangeCount > 0) {
+        selection.removeAllRanges();
+    }
+    selection.addRange(range);
+    document.execCommand('copy');
+    //alert("复制成功")
+    return true;
+    } /* [End] supports for old browsers [enD] */
 }
 
 window.loadjs=window.loadJS=(function(){
