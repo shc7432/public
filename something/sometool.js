db0b1f4320f88f1d3bd85b8a113720aef623bf1d
shc0743 <shc0743@outlook.com>
2024-12-31 19:45:19 +0800

    Update sometool.js

--- something/sometool.js
+++ something/sometool.js
@@ -1,6 +1,5 @@
 /********
 DATE=2020-03-22
-LASTDATE=2020-05-06
 ********/
 
 /****
@@ -10,180 +9,180 @@ Are you want test your code's error?You can use this link:
 <script src="https://shc7432.github.io/public/getLinkInput/testcode.js"></script>
 ****/
 
-(function(){
+(function () {
 
-//"use strict";
+  //"use strict";
 
-Number.prototype.pow=function(a){
-if(isNaN(a)) a=1;
-return (this**a);
-}
-String.prototype.showByAlert=function(){
-alert(this);
-}
-String.prototype.dmwrite=function(){
-document.write(this);
-}
-Object.prototype.clone=function(){
-let a=JSON.stringify(this);
-return JSON.parse(a);
-}
-NodeList.prototype.forEach = function (callback) {
-  Array.prototype.forEach.call(this, callback);
-}
-Object.defineProperty(Number.prototype, "pow", {
-  writable: false,
-  enumerable: false,
-  configurable: true
-});
-Object.defineProperty(String.prototype, "showByAlert", {
-  writable: false,
-  enumerable: false,
-  configurable: true
-});
-Object.defineProperty(String.prototype, "dmwrite", {
-  writable: false,
-  enumerable: false,
-  configurable: true
-});
-Object.defineProperty(Object.prototype, "clone", {
-  writable: false,
-  enumerable: false,
-  configurable: true
-});
-Object.defineProperty(NodeList.prototype, "forEach", {
-  writable: false,
-  enumerable: false,
-  configurable: true
-});
+  Number.prototype.pow = function (a) {
+    if (isNaN(a)) a = 1;
+    return (this ** a);
+  }
+  String.prototype.showByAlert = function () {
+    alert(this);
+  }
+  String.prototype.dmwrite = function () {
+    document.write(this);
+  }
+  Object.prototype.clone = function () {
+    let a = JSON.stringify(this);
+    return JSON.parse(a);
+  }
+  NodeList.prototype.forEach = function (callback) {
+    Array.prototype.forEach.call(this, callback);
+  }
+  Object.defineProperty(Number.prototype, "pow", {
+    writable: false,
+    enumerable: false,
+    configurable: true
+  });
+  Object.defineProperty(String.prototype, "showByAlert", {
+    writable: false,
+    enumerable: false,
+    configurable: true
+  });
+  Object.defineProperty(String.prototype, "dmwrite", {
+    writable: false,
+    enumerable: false,
+    configurable: true
+  });
+  Object.defineProperty(Object.prototype, "clone", {
+    writable: false,
+    enumerable: false,
+    configurable: true
+  });
+  Object.defineProperty(NodeList.prototype, "forEach", {
+    writable: false,
+    enumerable: false,
+    configurable: true
+  });
 
-/*
-PsTools:
-https://download.sysinternals.com/files/PSTools.zip
-*/
+  /*
+  PsTools:
+  https://download.sysinternals.com/files/PSTools.zip
+  */
 
-window.copytext=function(text){
-     /* support for new browsers */
+  window.copytext = function (text) {
+    /* support for new browsers */
     if (window.navigator.clipboard) {
-        return window.navigator.clipboard.writeText(text);
+      return window.navigator.clipboard.writeText(text);
     }
-    
+
     /* supports for old browsers */ {
-    if (typeof document.execCommand !== "function") {
+      if (typeof document.execCommand !== "function") {
         //alert("复制失败，请长按复制");
         return false;
-    }
-    var dom = document.createElement("textarea");
-    dom.value = text;
-    dom.setAttribute('style', 'position:absolute;top:-1000%;');
-    if (document.body) {
+      }
+      var dom = document.createElement("textarea");
+      dom.value = text;
+      dom.setAttribute('style', 'position:absolute;top:-1000%;');
+      if (document.body) {
         document.body.appendChild(dom);
-    } else if (document.documentElement) {
+      } else if (document.documentElement) {
         document.documentElement.appendChild(dom);
-    } else {
+      } else {
         //alert("复制失败，请长按复制");
         return false;
-    }
-    dom.select();
-    var result = document.execCommand('copy');
-    if (document.body) {
+      }
+      dom.select();
+      var result = document.execCommand('copy');
+      if (document.body) {
         document.body.removeChild(dom);
-    } else if (document.documentElement) {
+      } else if (document.documentElement) {
         document.documentElement.removeChild(dom);
-    } else {
+      } else {
         //alert("复制失败，请长按复制");
         return false;
-    }
-    if (result) {
+      }
+      if (result) {
         //alert("复制成功");
         return true;
-    }
-    if (typeof document.createRange !== "function") {
+      }
+      if (typeof document.createRange !== "function") {
         //alert("复制失败，请长按复制");
         return false;
-    }
-    var range = document.createRange();
-    var div = document.createElement('div');
-    div.innerHTML = text;
-    div.setAttribute('style', 'position:absolute;top:-1000%;');
-    if (document.body) {
+      }
+      var range = document.createRange();
+      var div = document.createElement('div');
+      div.innerHTML = text;
+      div.setAttribute('style', 'position:absolute;top:-1000%;');
+      if (document.body) {
         document.body.appendChild(div);
-    } else if (document.documentElement) {
+      } else if (document.documentElement) {
         document.documentElement.appendChild(div);
-    } else {
+      } else {
         //alert("复制失败，请长按复制");
         return false;
-    }
-    range.selectNode(div);
-    const selection = window.getSelection();
-    if (selection.rangeCount > 0) {
+      }
+      range.selectNode(div);
+      const selection = window.getSelection();
+      if (selection.rangeCount > 0) {
         selection.removeAllRanges();
-    }
-    selection.addRange(range);
-    document.execCommand('copy');
-    //alert("复制成功")
-    return true;
+      }
+      selection.addRange(range);
+      document.execCommand('copy');
+      //alert("复制成功")
+      return true;
     } /* [End] supports for old browsers [enD] */
-}
+  }
 
-window.loadjs=window.loadJS=(function(){
-    var head = document.getElementsByTagName('head')[0]; 
-    var script= document.createElement("script"); 
-    script.type = "text/javascript"; 
-    script.src=arguments[0]; 
-    head.appendChild(script); 
-    if(/ie/i.test(navigator.userAgent)) return script;
-    script.onload=arguments[1];
-    if(typeof arguments[1] != "function" ) script.onload=undefined;
+  window.loadjs = window.loadJS = (function () {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = arguments[0];
+    head.appendChild(script);
+    if (/ie/i.test(navigator.userAgent)) return script;
+    script.onload = arguments[1];
+    if (typeof arguments[1] != "function") script.onload = undefined;
     return script;
-})
-String.prototype.loadJS=String.prototype.loadjs=
-String.prototype.loadJSFromThis=function(){
-    var head = document.getElementsByTagName('head')[0]; 
-    var script= document.createElement("script"); 
-    script.type = "text/javascript"; 
-    script.src=this; 
-    head.appendChild(script); 
-}
-Object.prototype.loadJS=Object.prototype.loadjs=
-Object.prototype.loadJSFromThis=function(){
-    var head = document.getElementsByTagName('head')[0]; 
-    var script= document.createElement("script"); 
-    script.type = "text/javascript"; 
-    script.src=this.src; 
-    head.appendChild(script); 
-    if(/ie/i.test(navigator.userAgent)) return script;
-    script.onload=this.callback;
-    if(typeof this.callback != "function" ) script.onload=undefined;
-    return script;
-}
+  })
+  String.prototype.loadJS = String.prototype.loadjs =
+    String.prototype.loadJSFromThis = function () {
+      var head = document.getElementsByTagName('head')[0];
+      var script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = this;
+      head.appendChild(script);
+    }
+  Object.prototype.loadJS = Object.prototype.loadjs =
+    Object.prototype.loadJSFromThis = function () {
+      var head = document.getElementsByTagName('head')[0];
+      var script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = this.src;
+      head.appendChild(script);
+      if (/ie/i.test(navigator.userAgent)) return script;
+      script.onload = this.callback;
+      if (typeof this.callback != "function") script.onload = undefined;
+      return script;
+    };
 
-(function(){
+      (function () {
 
-window.pwTools=new Object();
-Object.defineProperty(window, "pwTools", {
-  writable: false,
-  enumerable: true,
-  configurable: true
-});
+        window.pwTools = new Object();
+        Object.defineProperty(window, "pwTools", {
+          writable: false,
+          enumerable: true,
+          configurable: true
+        });
 
-pwTools.base64=new Object();
-pwTools.base64.add=function(str){
-  return btoa(encodeURIComponent(str));
-}
-pwTools.base64.unadd=function(str){
-  return decodeURIComponent(atob(str));
-}
-Object.defineProperty(pwTools, "base64", {
-  writable: false,
-  enumerable: true,
-  configurable: true
-});
+        pwTools.base64 = new Object();
+        pwTools.base64.add = function (str) {
+          return btoa(encodeURIComponent(str));
+        }
+        pwTools.base64.unadd = function (str) {
+          return decodeURIComponent(atob(str));
+        }
+        Object.defineProperty(pwTools, "base64", {
+          writable: false,
+          enumerable: true,
+          configurable: true
+        });
 
-})()
+      })();
 
-function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
+  function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
 })()
